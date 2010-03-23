aed1125601b607e0fcf5121b1346f82ab12b371d
Piotr Macuk <piotr@macuk.pl>
2010-03-23 08:54:59 +0100

    Nip class and tests

--- lib/nip.rb
+++ lib/nip.rb
@@ -1,1 +1,44 @@
-# Nip
+class Nip
+  def initialize(number)
+    @number = number.to_s.gsub(/-/, '')
+    @valid = validate
+  end
+
+  def to_s
+    @number
+  end
+
+  def valid?
+    @valid
+  end
+
+  private
+    def validate
+      has_proper_length and \
+        contains_only_digits and \
+        has_proper_control_digit
+    end
+
+    def has_proper_length
+      @number.size == 10
+    end
+
+    def contains_only_digits
+      @number =~ /^\d+$/
+    end
+
+    def weights
+      [6, 5, 7, 2, 3, 4, 5, 6, 7]
+    end
+
+    def has_proper_control_digit
+      digits = @number.split('').map { |i| i.to_i }
+      i = 0; sum = 0
+      digits[0..-2].each do |d| 
+        sum += (d * weights[i])
+        i += 1
+      end
+      ctrl = (sum % 11) % 10
+      ctrl == digits[-1]
+    end
+end
--- test/nip_test.rb
+++ test/nip_test.rb
@@ -1,8 +1,25 @@
 require 'test_helper'
 
 class NipTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  Valid   = %w(845-167-08-82 583-10-23-182 5840955985)
+  Invalid = %w(123 845-167-00-00 000-111-22-33)
+
+  test "number and to_s methods" do
+    n = Nip.new('845-167-08-82')
+    assert '8451670882', n.to_s
+    n = Nip.new(8451670882)
+    assert '8451670882', n.to_s
+  end
+
+  test "valid? method" do
+    Valid.each do |number|
+      n = Nip.new(number)
+      assert n.valid?
+    end
+
+    Invalid.each do |number|
+      n = Nip.new(number)
+      assert_equal false, n.valid?
+    end
   end
 end
